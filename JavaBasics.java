6872ff2885b54a9f2f6a000fcb84594df52282b4
Dhawal <dhwalgala05@gmail.com>
2025-10-29 00:54:05 +0530

    basic sum and multiply

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,15 +1,11 @@
 import java.util.*;
 
 public class JavaBasics {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-       String input = sc.next(); // space dene ke baad age ka sara ingore hojayega
-    //     System.out.println(input);
-    //    String name =  sc.nextLine();// to store spaces also
-    //     System.out.println();
-
-        float price  = sc.nextFloat();
-        System.out.println(price);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println(product);
     }
 }
