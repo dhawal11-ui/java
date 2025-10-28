f539d4e39b1331682c6b65b251f4200cde31f109
Dhawal <dhwalgala05@gmail.com>
2025-10-29 01:00:38 +0530

    area of circle using float . resolving error related to double conversion

--- JavaBasics.java
+++ JavaBasics.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int product = a * b;
-        System.out.println(product);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
     }
 }
