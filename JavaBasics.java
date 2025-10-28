78035a1e9399cbaa45971ac41176a39d5cc32988
dhawal11-ui <dhawalgala05@gmail.com>
2025-10-28 18:09:02 +0530

    basic code

---
+++ JavaBasics.java
@@ -0,0 +1,11 @@
+public class JavaBasics { // file and class ka na;me same hona mangta
+
+    public static void main(String[] args) {  // main() ko he java ka compiller pehele dhundata h
+        System.out.println("Hello meow\n meow");
+    }
+        
+        
+       
+         
+    }
+    
