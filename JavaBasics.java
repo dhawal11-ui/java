12c455e9a64c5487f9b41dfc75fbd6613be3ff06
Dhawal <dhwalgala05@gmail.com>
2025-10-29 10:51:36 +0530

    increament common doubts

--- JavaBasics.java
+++ JavaBasics.java
@@ -3,10 +3,9 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         int A = 10;
-        int B = 5;
-        System.out.println("add = " + (A + B)); // must use brackets
-        System.out.println("adde = " + A + B); // if bracket is not usesd the answer should be 105
-        System.out.println("substract = " + (A - B));
-        System.out.println("d8iv = " + (A / B));
+        int B = A++; // pehele b banega then a++ hoga.
+        System.out.println(A);
+        System.out.println(B);
+
     }
 }
