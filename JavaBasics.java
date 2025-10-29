cfc98ad4aa84182444f87b77ac5bf8e8187d0fbd
Dhawal <dhwalgala05@gmail.com>
2025-10-29 11:48:17 +0530

    dono condn check kregfa upr ki true hoi to bhi . therefore, else if likho . compiler ki powwer bachegi . if sahi huea toh else wale koi v check nhi krega. code will run faster on big scale projects

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,15 +1,12 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        int age = 14;
-        if (age >= 18) {
-            System.out.println("you are an adult now");
+        int A = 2;
+
+        if (A % 2 == 0) {
+            System.out.println("even");
         }
-        if (age > 13 && age < 18) {
-            System.out.println("teenager"); // jo condn sahi hogyi uske badk8i sare conditions reject krdega and this is
-                                            // the fucking loophole
-        }
-        if (age < 10) {
-            System.out.println("not adult");
+        if (A % 2 == 0) {
+            System.out.println("odd");
         }
     }
 }
