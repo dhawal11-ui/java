bc0e903fe3c9309bbd460f9f239b86af09d24455
Dhawal <dhwalgala05@gmail.com>
2025-10-29 19:44:26 +0530

    basic Questions

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,36 +1,60 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void main(String[] args) { // code input lene ke bad age procced krega./ First A then B then Char .
-                                             // then switch . upto down approach
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("E4nter A");
-        int a = sc.nextInt();
-        System.out.println("Enter B");
-        int b = sc.nextInt();
-        System.out.println("Enter operator");
-        char operator = sc.next().charAt(0);
-        System.out.println("Your answer is ");
-        switch (operator) {
-            case '+':
-                System.out.println(a + b);
-                break;
-            case '-':
-                System.out.println(a - b);
-                break;
-            case '*':
-                System.out.println(a * b);
-                break;
-            case '/':
-                System.out.println(a / b);
-                break;
-            case '%':
-                System.out.println(a % b);
-                break;
+        // int A = sc.nextInt();
 
-            default:
-                System.out.println("wrong case");
+        // if (A > 0) {
+        // System.out.println("A is an positive NUmbeer");
+        // } else {
+        // System.out.println("A is an Negative NUmbeer");
+        // }
+
+        // double temp = 103.5;
+        // if (temp > 100) {
+        // System.out.println("YOu have fever");
+        // } else {
+        // System.out.println("No fever");
+        // }
+
+        // int week = 6;
+        // switch (week) {
+        // case 1:
+        // System.out.println("sunday");
+        // break;
+        // case 2:
+        // System.out.println("monday");
+        // break;
+        // case 3:
+        // System.out.println("tuesday");
+        // break;
+        // case 4:
+        // System.out.println("wensday");
+        // break;
+        // case 5:
+        // System.out.println("thursday");
+        // break;
+        // case 6:
+        // System.out.println("friday");
+        // break;
+        // case 7:
+        // System.out.println("saturday");
+        // break;
+
+        // default:
+        // System.out.println("you enter a wrong number");
+        // }
+
+        // x=f and y =a
+
+        int year = sc.nextInt();
+
+        if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+            System.out.println("leap year");
+        } else {
+            System.out.println("not a leap year");
         }
-
+        ;
     }
 }
