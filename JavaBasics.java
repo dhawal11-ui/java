ac72055ed44336e62b5e28c1ad2a4d0aad33245b
Dhawal <dhwalgala05@gmail.com>
2025-10-29 10:47:04 +0530

    brackets

--- JavaBasics.java
+++ JavaBasics.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        float marks = 99.999f;
-        int marks2 = (int) marks;
-        System.out.println(marks2);
+        int A = 10;
+        int B = 5;
+        System.out.println("add = " + (A + B)); // must use brackets
+        System.out.println("adde = " + A + B); // if bracket is not usesd the answer should be 105
+        System.out.println("substract = " + (A - B));
+        System.out.println("d8iv = " + (A / B));
     }
 }
