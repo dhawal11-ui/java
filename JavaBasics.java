027d3d1f1a9f782ddd75582ae78f45ba3b099fef
Dhawal <dhwalgala05@gmail.com>
2025-10-29 19:20:05 +0530

    making a simple calculator

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,35 +1,36 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void main(String[] args) {
-        char ch = 'a';
+    public static void main(String[] args) { // code input lene ke bad age procced krega./ First A then B then Char .
+                                             // then switch . upto down approach
+        Scanner sc = new Scanner(System.in);
+        System.out.println("E4nter A");
+        int a = sc.nextInt();
+        System.out.println("Enter B");
+        int b = sc.nextInt();
+        System.out.println("Enter operator");
+        char operator = sc.next().charAt(0);
+        System.out.println("Your answer is ");
+        switch (operator) {
+            case '+':
+                System.out.println(a + b);
+                break;
+            case '-':
+                System.out.println(a - b);
+                break;
+            case '*':
+                System.out.println(a * b);
+                break;
+            case '/':
+                System.out.println(a / b);
+                break;
+            case '%':
+                System.out.println(a % b);
+                break;
 
-        switch (ch) {
-            case 'a':
-                System.out.println("samosa");
-                break;
-            case 'b':
-                System.out.println("burger");
-                break;
-
-            case 'f':
-                System.out.println("mango shake");
-                break;
             default:
-                System.out.println("we relize we are dreami9ng");
-
+                System.out.println("wrong case");
         }
+
     }
 }
-/*
- * o/p without using break.
- * burger
- * mango shake
- * we relize we are dreami9ng
- */
-
-/*
- * o/p using break.
- * burger
- * 
- */
