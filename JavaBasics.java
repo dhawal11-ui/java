80daaac82c012d4791ce614fb9ba7e9c99c9b37b
Dhawal <dhwalgala05@gmail.com>
2025-10-31 22:57:30 +0530

    nested loop for starts *,**,***

--- JavaBasics.java
+++ JavaBasics.java
@@ -4,25 +4,12 @@ public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n == 2) {
-            System.out.println("n is prime");
-        } else {
-            boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(n); i++) { // square root of no. n
-                if (n % i == 0) {
-                    isPrime = false;
-                    break;
-                }
-                ;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
-            if (isPrime == true) {
-                System.out.println("n is prime");
-            } else {
-                System.out.println("n is not prime ");
-            }
-
+            System.out.println("\n");
         }
-
     }
 
 }
