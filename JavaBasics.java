605d712f243975b4f64e43b2af5b63138a359bd6
Dhawal <dhwalgala05@gmail.com>
2025-10-31 22:27:18 +0530

    prime or not prime by a different approach.

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,60 +1,28 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // int A = sc.nextInt();
-
-        // if (A > 0) {
-        // System.out.println("A is an positive NUmbeer");
-        // } else {
-        // System.out.println("A is an Negative NUmbeer");
-        // }
-
-        // double temp = 103.5;
-        // if (temp > 100) {
-        // System.out.println("YOu have fever");
-        // } else {
-        // System.out.println("No fever");
-        // }
-
-        // int week = 6;
-        // switch (week) {
-        // case 1:
-        // System.out.println("sunday");
-        // break;
-        // case 2:
-        // System.out.println("monday");
-        // break;
-        // case 3:
-        // System.out.println("tuesday");
-        // break;
-        // case 4:
-        // System.out.println("wensday");
-        // break;
-        // case 5:
-        // System.out.println("thursday");
-        // break;
-        // case 6:
-        // System.out.println("friday");
-        // break;
-        // case 7:
-        // System.out.println("saturday");
-        // break;
-
-        // default:
-        // System.out.println("you enter a wrong number");
-        // }
-
-        // x=f and y =a
-
-        int year = sc.nextInt();
-
-        if ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
-            System.out.println("leap year");
+        int n = sc.nextInt();
+        if (n == 2) {
+            System.out.println("n is prime");
         } else {
-            System.out.println("not a leap year");
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(n); i++) { // square root of no. n
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+                ;
+            }
+            if (isPrime == true) {
+                System.out.println("n is prime");
+            } else {
+                System.out.println("n is not prime ");
+            }
+
         }
-        ;
+
     }
+
 }
