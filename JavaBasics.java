115ef68ef721224960be87b7b7d2352bbc21656e
Dhawal <dhwalgala05@gmail.com>
2025-11-07 00:53:03 +0530

    avg of num

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,24 +1,9 @@
-
 public class JavaBasics {
-
-    public static void deciToBi(int n) {
-        int pow = 0;
-        int biNum = 0;
-
-        while (n > 0) {
-            int rem = n % 2;
-            biNum = biNum + rem * (int) Math.pow(10, pow);
-
-            pow++;
-            n = n / 2;
-
-        }
-        System.out.println(biNum);
-
+    public static int avg(int a,int b,int c) {
+        return (a+b+c)/3;
     }
-
     public static void main(String[] args) {
-        deciToBi(7);
+      System.out.println(avg(2,3,4));  
     }
 
 }
