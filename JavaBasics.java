4313f19d6aa211f3f71e2e59f0f2a19504f74a5f
Dhawal <dhwalgala05@gmail.com>
2025-11-07 21:14:13 +0530

    my worng code

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,13 +1,28 @@
-public class JavaBasics {
-    public static boolean isEven(int n) {
-        if(n%2==0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    public static void main(String[] args) {
-        System.out.println(isEven(2));
-    }
+public class JavaBasics { 
 
-}
+    public static void main(String[] args) {  
+      int line ;
+      int numLine = 4;
+      int numCol = 4;
+
+    for(line=1;line<=numLine;line++){
+        
+        if(line==1||line==numLine){
+           for(int j=1; j<= numCol;j++){
+            System.out.print("*");
+           }
+        }else{
+            System.out.println("\n");
+            for(int k=1;k<=numCol;k++){
+               if(k==1||k==numCol){
+                System.out.print("*");
+               }else{
+                System.out.print("a");
+               }
+            }
+        }
+
+    }
+        
+    }
+    }
