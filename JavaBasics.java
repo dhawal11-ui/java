347e2a8fee75b00d4a602f167289b4e3bf57fa0d
Dhawal <dhwalgala05@gmail.com>
2025-11-09 00:06:15 +0530

    array ke jo changes bhar hue whi changes main meh reflect krege . I think becoz other function does not make the copy of the array . It declares changes in the original array .

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,28 +1,19 @@
-public class JavaBasics { 
+import java.util.*;
 
-    public static void main(String[] args) {  
-      int line ;
-      int numLine = 4;
-      int numCol = 4;
-
-    for(line=1;line<=numLine;line++){
+public class JavaBasics {
+    public static void update(int game[]) {
+        for(int i=0;i<=game.length-1;i++){
+         game[i]++;    
+        }
         
-        if(line==1||line==numLine){
-           for(int j=1; j<= numCol;j++){
-            System.out.print("*");
-           }
-        }else{
-            System.out.println("\n");
-            for(int k=1;k<=numCol;k++){
-               if(k==1||k==numCol){
-                System.out.print("*");
-               }else{
-                System.out.print("a");
-               }
-            }
+    }
+    public static void main(String args[]){
+      int marks[] = {97,98,99};
+      update(marks);
+
+      for(int i=0;i<=marks.length;i++){
+         System.out.println(marks[i]);    
         }
 
     }
-        
-    }
-    }
+} 
