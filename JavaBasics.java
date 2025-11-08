bdc9aa78975b20cb54e943b3d9c173dfadbad4ff
Dhawal <dhwalgala05@gmail.com>
2025-11-09 00:08:30 +0530

    not an array does not chnages in main .

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void update(int game[]) {
+    public static void update(int game[],int n) {
+        n=10;
         for(int i=0;i<=game.length-1;i++){
          game[i]++;    
         }
@@ -9,7 +10,9 @@ public class JavaBasics {
     }
     public static void main(String args[]){
       int marks[] = {97,98,99};
-      update(marks);
+      int nonChangable =5;
+      update(marks,nonChangable);
+      System.out.println(nonChangable);
 
       for(int i=0;i<=marks.length;i++){
          System.out.println(marks[i]);    
