b9eef9f56af90fbe95b8d6bcd221c1c6dbddcc92
Dhawal <dhwalgala05@gmail.com>
2025-11-09 23:35:25 +0530

    pairs in arry and total number of pairs .

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,27 +1,30 @@
 public class JavaBasics {
-
-    public static void reverse(int numbers[]) {
-        int first = 0 ,  last = numbers.length -1;
-
-        while (first < last) {
-            //swap
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
-            
-
-        first++;
-        last --;
-        }
-        
-    }
-        public static void main(String args[]){
-    int numbers[] = {2,4,6,8,10};
-            reverse(numbers);
-            for(int i=0; i< numbers.length ; i++){
-                System.out.print(numbers[i] + " ");
+    public static void printPairs(int numbers[]) {
+        int  tp = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i];
+            for (int j = i + 1; j < numbers.length; j++) {
+                
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+                tp++;
             }
             System.out.println();
+           
+        }
+        System.out.println(tp);
     }
-} 
 
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printPairs(numbers);
+    }
+
+}
+
+
+// formula to find by observing patterrsn . n(n-2)/2; .. sum of n-1 numbners .
+
+// outer loop -> o(n)
+// inner loop - > o(n  + n-1 ... 1)
+
+// time complexity at worst case -> o(n2)
