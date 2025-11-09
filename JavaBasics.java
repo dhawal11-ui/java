9b1b6c8ebdc6b261dd5b03c90284f5b080aa9328
Dhawal <dhwalgala05@gmail.com>
2025-11-09 13:06:15 +0530

    finding largest procedure for smallest

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,22 +1,20 @@
 import java.util.*;
 
 public class JavaBasics {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE; // -infuinity
 
-    public static int  search(int numbers[],int key) {
-        for(int i=0 ; i<numbers.length; i++){
-            if(numbers[i]==key){
-                return i;
+        for(int i=0 ; i<numbers.length ; i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
             }
+
         }
-        return -1;
+
+        return largest;
     }
     public static void main(String args[]){
-      int numbers[] = {1,2,3,4,5,6,7,8,9};
-      int key = 5;
-      int index = search(numbers, key);
-
-      System.out.println(index);
-
-
+        int numbers[] = {1,2,3,94,5 };
+       System.out.println(getLargest(numbers));
     }
 } 
