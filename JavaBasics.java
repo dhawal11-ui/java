7fe3d7b3b6ba44021617cdacc22dc86156e4f54b
Dhawal <dhwalgala05@gmail.com>
2025-11-23 21:59:08 +0530

    .....

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,46 +1,39 @@
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class JavaBasics {
 
-    public static void printSubArrays(int numbers[]) {
-        int currSum = 0, maxSum = Integer.MIN_VALUE, prefix[] = new int[numbers.length];
-        // calculate prefix array.
-        for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i - 1] + numbers[i];
-        }
+    public static void numCheck(int num[]){
+           for(int i=0;i<num.length;i++){
+            int checkNo = num[i];
 
-        for (int i = 0; i < numbers.length; i++) { // for determining starting position of indexes .
-            int start = i;
-            for (int j = i; j < numbers.length; j++) { // start se end dono ki positon define krna .
-                int end = j;
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
-                if (maxSum < currSum) {
-                    maxSum = currSum;
+                for(int j=0;j<num.length;j++){
+                    if(checkNo == num[j]){
+                     System.out.println("True3");
+                     break;
+
+                    }
+
                 }
-
-            }
-
         }
-        System.out.println("Max sum = " + maxSum);
     }
 
-    public static void kadanes(int numbers[]){ // what if al nos . are negetive // hw wirte special case ., 
-        int ms = Integer.MIN_VALUE;
-        int cs = 0;
-
-        for(int i=0;i< numbers.length;i++){
-            cs += numbers[i];
-
-            if(cs < 0){
-                cs = 0;
-            }
-            ms = Math.max(cs,ms);
-        }
-        System.out.println("our max subarry sum is :"+ ms);
+    public static int add(int a, int b) {
+        return a + b;
     }
     public static void main(String args[]) {
-        int numbers[] = { -2,-3,4,-1,-2,1,5,-3 };
-        kadanes(numbers);
-    }
+        int num[]={1, 2, 3, 1};
+        numCheck(num);
 
+        // Test the add function
+        int result = add(5, 3);
+        System.out.println("Addition result: " + result);
+
+        // Add an array of 5 natural numbers and sum them
+        int[] naturalNumbers = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int number : naturalNumbers) {
+            sum += number;
+        }
+        System.out.println("Sum of natural numbers: " + sum);
+
+    }
+    
 }
---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO for Adding Addition Function in Java
+
+- [x] Add a static method `add` to JavaBasics.java that takes two integers and returns their sum.
+- [x] Compile the Java code to ensure no syntax errors.
+- [x] Run the program to verify the addition function works correctly.
+s
