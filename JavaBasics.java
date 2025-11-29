6ba7d3849761ffaf06adbb4ce59f643fb6b9aff8
Dhawal <dhwalgala05@gmail.com>
2025-11-29 22:42:10 +0530

    clear / remove a bit from a positoon ... CLEar ith bit

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,23 +1,34 @@
 public class JavaBasics {
-    public static void oddOrEven(int n){
-    int bitMask = 1;
-    if((n & bitMask) ==0){ // last bit nikaltaa hai & bitmask(1) . 0 rhi to odd 1 rhi to even l.
-        System.out.println("even no");
-    }else{
-        System.out.println("odd number");
+    public static void oddOrEven(int n) {
+        int bitMask = 1;
+        if ((n & bitMask) == 0) { // last bit nikaltaa hai & bitmask(1) . 0 rhi to odd 1 rhi to even l.
+            System.out.println("even no");
+        } else {
+            System.out.println("odd number");
+        }
     }
-}
 
-    public static int getIthBit(int n,int i){
-        int bitMask = 1<<i;
-        if((n&bitMask)==0){
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
             return 0;
-        }else{
+        } else {
             return 1;
         }
     }
+
+    public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask; 
+    }
+
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+
     public static void main(String[] args) {
-       System.out.println(getIthBit(10, 2));   // 10--> 1010 ; i=2 , index 2 from zeroth place .. 0-1-2... | i=2=> 0;
-       // answer =0;
+        System.out.println(clearIthBit(10, 1));
     }
 }
