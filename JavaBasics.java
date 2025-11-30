ccddced47cf10c5e556f78265865d93e38b71d3e
Dhawal <dhwalgala05@gmail.com>
2025-11-30 21:31:21 +0530

    ,

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,8 +1,9 @@
 public class JavaBasics {
-    public static boolean isPowerOfTwo(int n){
-        return (n & (n-1)) ==0 ;  // propety of binary num .
-// also can we do it by taking log.?
+    public static boolean isPowerOfTwo(int n) {
+        return (n & (n - 1)) == 0; // propety of binary num .
+        // also can we do it by taking log.?
     }
+
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(3434));
     }
