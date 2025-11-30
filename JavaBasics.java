8b52ed36b19bf65516f9b8e6be1e992337bf71f1
Dhawal <dhwalgala05@gmail.com>
2025-11-30 12:29:14 +0530

    check if the number is power of 2 or not

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,61 +1,9 @@
 public class JavaBasics {
-    public static void oddOrEven(int n) {
-        int bitMask = 1;
-        if ((n & bitMask) == 0) { // last bit nikaltaa hai & bitmask(1) . 0 rhi to odd 1 rhi to even l.
-            System.out.println("even no");
-        } else {
-            System.out.println("odd number");
-        }
+    public static boolean isPowerOfTwo(int n){
+        return (n & (n-1)) ==0 ;  // propety of binary num .
+// also can we do it by taking log.?
     }
-
-    public static int getIthBit(int n, int i) {
-        int bitMask = 1 << i;
-        if ((n & bitMask) == 0) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
-
-    public static int setIthBit(int n, int i) {
-        int bitMask = 1 << i;
-        return n | bitMask;
-    }
-
-    public static int clearIthBit(int n, int i) {
-        int bitMask = ~(1 << i);
-        return n & bitMask;
-    }
-
-    public static int clearIthBits(int n, int i) {
-        int bitMask = (~0) << i;
-        return n & bitMask;
-    }
-
-    public static int updateIthBit(int n, int i, int newBit) {
-        // if (newBit == 0) {
-        // return clearIthBit(n, i);
-
-        // } else {
-        // return setIthBit(n, i);
-        // }
-
-        // approach 2
-
-        n = clearIthBit(n, i);
-        int BitMask = newBit << i;
-        return n | BitMask;
-
-    }
-
-    public static int clearIBits(int n , int i, int j){
-        int a = ((~0)<<(j+1));
-        int b = (1<<i)-1;
-        int bitMask = a|b;
-        return n& bitMask;
-    }
-
     public static void main(String[] args) {
-        System.out.println(clearIBits(10,2,4));
+        System.out.println(isPowerOfTwo(3434));
     }
 }
