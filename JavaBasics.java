5c2583aa1247a556ee5bcf21d98c3cbfd02498e8
Dhawal <dhwalgala05@gmail.com>
2025-12-02 22:16:24 +0530

    multiple constructors.CONSTRUCTOR OVERLOADING (same name but parameter alag dalne se alag Student() calll hw=uwa

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,8 +1,13 @@
 public class JavaBasics {
 
     public static void main(String[] args) {
-        Student s1 = new Student("dhawal"); // student class ke andr ka student fu
-        System.out.println(s1.name);
+        // CONSTRUCTOR OVERLOADING (ex of polymorphism)
+        Student s1 = new Student(); // fn1 ..(agr yeh line chalaani hai bina student class meh constructor banaye
+                                    // yani default constructor made by java ko use krke . Toh hame baki sare
+                                    // construcors jo hane banaye class student meh woh hame delete karne hoge
+                                    // .bchncod)
+        Student s2 = new Student("dhawal"); // fn2
+        Student s3 = new Student(19);// fn3
 
     }
 }
@@ -11,7 +16,15 @@ class Student {
     String name;
     int roll;
 
-    Student(String name) {
+    Student() { // fn1
+        System.out.println("constructor is called");
+    }
+
+    Student(int roll) {// fn2
+        this.roll = roll;
+    }
+
+    Student(String name) {// fn3
         this.name = name;
     }
 }
