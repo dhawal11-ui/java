5bec1b6fd7a1e425c27fc85b2f6801efd22374c3
Dhawal <dhwalgala05@gmail.com>
2025-12-03 00:48:35 +0530

    Method overrriding . apne class ka he function call krega . Name chahe same v ho ya alag v ho...

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,75 +1,22 @@
 public class JavaBasics {
 
     public static void main(String[] args) {
-        Peacock p1 = new Peacock();
-        p1.eat();
+        Deer d = new Deer();
+        d.eat();
+        Animal a = new Animal();
+        a.eat();
+
     }
 }
 
-// Base Class
 class Animal {
-    String color;
-
     void eat() {
-        System.out.println("eats");
-    }
-
-    void breadth() {
-        System.out.println("breadths");
-    }
-
-}
-//Mammals
-class Mammal extends Animal {
-    void walk() {
-        System.out.println("walks");
+        System.out.println("eats anything");
     }
 }
 
-class Dog extends Animal{
-    void name(){
-        System.out.println("Dog");
-    }
-}
-class Cat extends Animal{
-    void name(){
-        System.out.println("Cat");
-    }
-}
-class Human extends Animal{
-    void name(){
-        System.out.println("Human");
-    }
-}
-
-//Fish
-class Fish extends Animal {
-    void swim() {
-        System.out.println("Swim");
-    }
-}
-
-class Tuna extends Fish {
-    void name() {
-        System.out.println("Tuna");
-    }
-}
-
-class Shark extends Fish {
-    void name() {
-        System.out.println("Shark");
-    }
-}
-
-// Bird
-class Bird extends Animal {
-    void fly() {
-        System.out.println("FLy");
-    }
-}
-
-class Peacock extends Fish {
-    void name(){
-        System.out.println("peacock");
+class Deer extends Animal {
+    void eat() {
+        System.out.println("eats grass");
     }
 }
