1fc1f7ccb930f3f1ef69d08ce59ddb8d76adfbff
Dhawal <dhwalgala05@gmail.com>
2025-12-03 00:17:46 +0530

    Hybrid Inheritence

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,7 +1,8 @@
 public class JavaBasics {
 
     public static void main(String[] args) {
-        
+        Peacock p1 = new Peacock();
+        p1.eat();
     }
 }
 
@@ -18,21 +19,57 @@ class Animal {
     }
 
 }
-
+//Mammals
 class Mammal extends Animal {
     void walk() {
         System.out.println("walks");
     }
 }
 
+class Dog extends Animal{
+    void name(){
+        System.out.println("Dog");
+    }
+}
+class Cat extends Animal{
+    void name(){
+        System.out.println("Cat");
+    }
+}
+class Human extends Animal{
+    void name(){
+        System.out.println("Human");
+    }
+}
+
+//Fish
 class Fish extends Animal {
     void swim() {
         System.out.println("Swim");
     }
 }
 
+class Tuna extends Fish {
+    void name() {
+        System.out.println("Tuna");
+    }
+}
+
+class Shark extends Fish {
+    void name() {
+        System.out.println("Shark");
+    }
+}
+
+// Bird
 class Bird extends Animal {
     void fly() {
         System.out.println("FLy");
+    }
+}
+
+class Peacock extends Fish {
+    void name(){
+        System.out.println("peacock");
     }
 }
