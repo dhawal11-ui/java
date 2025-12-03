824158203621da98216e65e991db8a6b3e52a32c
Dhawal <dhwalgala05@gmail.com>
2025-12-03 23:08:12 +0530

    Refactor abstract class structure and update main method to demonstrate inheritance with Horse and Chicken classes. animals ne fucntions define kiya jo sabper apply ho . abstracct class . But animal ko copy nhi kar sakte. calling constructor hireachy call pata hona chahiye order

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,22 +1,59 @@
+// Abstract class
+/*
+ * 1>cannot create an instance(obj)of abstract class
+ * 2> abstract and non abstract method aajayege
+ * 3>
+ * can have constructors
+ */
+
 public class JavaBasics {
 
     public static void main(String[] args) {
-        Deer d = new Deer();
-        d.eat();
-        Animal a = new Animal();
-        a.eat();
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
 
+        System.out.println(h.color);
+
+        // Animal a = new Animal(); // cannot create instatntiate the type animal (err)
+        // {abstract class hai re }
     }
 }
 
-class Animal {
+abstract class Animal {
+    String color;
+
+    Animal() {
+        this.color = "brown";
+    }
+
     void eat() {
-        System.out.println("eats anything");
+        System.out.println("Animal eats");
+    }
+
+    // if you try Animal a = new Animal(); when Animal is abstract, the compiler
+    // will
+    // error — that’s the primary purpose of abstract here: prevent instantiation of
+    // the base type.
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void changeColor() {
+        this.color = "dark Brown";
+    }
+
+    void walk() {
+        System.out.println("walks on 4 legs");
     }
 }
 
-class Deer extends Animal {
-    void eat() {
-        System.out.println("eats grass");
+class Chicken extends Animal {
+    void changeColor() {
+        this.color = "yellow";
+    }
+
+    void walk() {
+        System.out.println("walks on 2 legs");
     }
 }
