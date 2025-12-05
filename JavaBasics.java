1def74a8099eb40755e76300a8c70e512ed0f3e8
Dhawal <dhwalgala05@gmail.com>
2025-12-05 07:29:51 +0530

    static keyword

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,44 +1,37 @@
-// - Multiple inheritence (5th).
-// - To achieve total abstraction. (100% abstraction)
-// - Define via interface keyword and copy properties in child via implements keyword .
-// - All methods are public , abstract, without implementation(no function define in code only name of the fucntion.) .
-//- Variables in interfaces are final , public , static.
+// - Static Keyword.
+// - Static keyword is java is uesd to share the same variable or method of a class with all the objects of the class.
+//  properties , fucntiosn , block , nested class can be declared as static in java.
+// static variable alag se created in the memory only once and sare objects uski reference ko point karte hain.
+
 public class JavaBasics {
     public static void main(String[] args) {
-        Queen q = new Queen();
-        q.moves();
+        Student s1 = new Student();
+        s1.schoolName = "JMV";
+
+        Student s2 = new Student();
+        System.out.println(s2.schoolName); // it will print JMV because schoolName is static variable shared by all
+                                           // objects of Student class
+
+        Student s3 = new Student();
+        s3.schoolName = "XYZ School";
     }
 }
 
-interface Herbivore {
-
-}
-interface Carnivore {
-
-}
-
-class Bear implements Herbivore , Carnivore {
-    
-}
-
-interface ChessPlayer {
-    void moves(); // bluprint.
-}
-
-class Queen implements ChessPlayer {
-    public void moves() {
-        System.err.println("up , down , left , right , diagonal(all sides) ");
+class Student {
+    static int returnPercentage(int math, int phy, int chem) { // logic har function ka same rhega . also recreate nhi
+                                                               // hoga toh memory v bachegi.
+        return (math + phy + chem) / 3;
     }
-}
 
-class Rook implements ChessPlayer {
-    public void moves() {
-        System.err.println("up , down , left , right  ");
+    String name;
+    int rollNo;
+    static String schoolName = "ABC School"; // static variable
+
+    void setName(String name) { // setters
+        this.name = name;
     }
-}
 
-class King implements ChessPlayer {
-    public void moves() {
-        System.err.println("up , down , left , right , diagonal by(1 step) ");
+    String getName() {// getters
+        return this.name;
     }
 }
