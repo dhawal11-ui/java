8d5961de5c71ee21f7a9177087aaeab956b390af
Dhawal <dhwalgala05@gmail.com>
2025-12-07 12:34:41 +0530

    error dega vehicle se car ka object call nhi kar sakte . Vehicle ke refrence variable ke pass utni space he nhi hai ki car ke object ko store kr sake.

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,11 +1,7 @@
 public class JavaBasics {
     public static void main(String[] args) {
-        Vehicle v = new Car(); // child ka object parent ke refrence variable meh access kar skate hai .
-        // Car c = new Vehicle(); // wrong
-        v.name();
-        // lhs refrence(point variable jo address store karta hai object ka)= rhs object
-        // (new keyword se ham memory dete hai). obj - meomory ke andar kuch data store
-        // huwa hai .
+        Vehicle v = new Car();
+        // v.name1(); 
 
     }
 }
@@ -13,8 +9,6 @@ public class JavaBasics {
 class Vehicle {
     String name;
 
-    // syso direct use nhi kar sakte hai usko construcotr ke andr karna padta
-    // bchnod.
     Vehicle() {
         System.out.print("Hello, ");
     }
@@ -27,11 +21,7 @@ class Vehicle {
 class Car extends Vehicle {
     String vehicleName;
 
-    void Car() {
-        System.out.println("vehicle called");
-    }
-
-    void name() {
+    void name1() {
         System.out.println("car is called ");
     }
 }
