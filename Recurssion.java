935961745d90975d456bfd195a7d742041c02ae7
Dhawal <dhwalgala05@gmail.com>
2025-12-08 16:28:05 +0530

    Factorial of an number uisng recurssion.

--- Recurssion.java
+++ Recurssion.java
@@ -1,9 +1,18 @@
 public class Recurssion {
 
     public static void main(String[] args) {
-        printInc(5);
+        
+        System.out.println(fact(5));
     }
-    
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n*fact(n-1);
+        return fn;
+    }
     public static void printInc(int n) {
         if(n ==1 ){
             System.out.print(n + " ");
