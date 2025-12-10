692297070c6db72314e00fe8addff30c44189937
Dhawal <dhwalgala05@gmail.com>
2025-12-10 13:57:23 +0530

    check array is sorted or not using recurssion

--- Recurssion.java
+++ Recurssion.java
@@ -1,23 +1,17 @@
 public class Recurssion {
-
-    public static void main(String[] args) {// fibonachi series
-        int n = 25;
-        System.out.println(fib(n));
+    public static void main(String[] args) {// sirf arrray check kr rhe hai . sorted bna nhi rhe hai array ko.
+        int arr[] = { 1, 2 };
+        System.out.println(isSorted(arr, 0));
     }
 
-    public static int fib(int n) {
-        // viswar rakho n-1 already calculate hojayega. (khud se likha but logic clear
-        // nhi hai )
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            int fibNMO = fib(n - 1);// recursion
-            int fibNMT = fib(n - 2);// recursion
-            int fibonachi = fibNMO + fibNMT;
-            return fibonachi;
+    public static boolean isSorted(int arr[], int i) { //
+        if (i == arr.length - 1) { // Base Case (for ending || and also for single element of array.)
+            return true;
         }
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+        return isSorted(arr, i + 1);
     }
 
 }
