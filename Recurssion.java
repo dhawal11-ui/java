5964ec59dd8644665b417c4db7210887bf2d1021
Dhawal <dhwalgala05@gmail.com>
2025-12-10 12:40:25 +0530

    fibonachi series

--- Recurssion.java
+++ Recurssion.java
@@ -1,15 +1,23 @@
 public class Recurssion {
 
-    public static void main(String[] args) {
-        System.out.println("you sum is " + sum(8));
+    public static void main(String[] args) {// fibonachi series
+        int n = 25;
+        System.out.println(fib(n));
     }
 
-    public static int sum(int n) {
-        if (n == 1) {// BC (base case).
+    public static int fib(int n) {
+        // viswar rakho n-1 already calculate hojayega. (khud se likha but logic clear
+        // nhi hai )
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
             return 1;
+        } else {
+            int fibNMO = fib(n - 1);// recursion
+            int fibNMT = fib(n - 2);// recursion
+            int fibonachi = fibNMO + fibNMT;
+            return fibonachi;
         }
-        int sum = sum(n - 1); // return store return store .
-        return sum + n;
     }
 
-} // time and space complexity are both O(n);.
+}
