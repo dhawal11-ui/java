aec11494839a1e98e5d4f5878185204898b65525
Dhawal <dhwalgala05@gmail.com>
2025-12-20 12:28:34 +0530

    first occurence of an element in an arrry . element refers to as key in this problem.

--- Recurssion.java
+++ Recurssion.java
@@ -1,17 +1,21 @@
 public class Recurssion {
-    public static void main(String[] args) {// sirf arrray check kr rhe hai . sorted bna nhi rhe hai array ko.
-        int arr[] = { 1, 2 };
-        System.out.println(isSorted(arr, 0));
+
+    public static void main(String[] args) { // To find first occurence of an element{key} in an array
+        int[] arr = { 1, 2, 3, 4, 6, 7, 8, 9, 5 };
+        System.out.println(firstOcc(arr, 5, 0));
     }
 
-    public static boolean isSorted(int arr[], int i) { //
-        if (i == arr.length - 1) { // Base Case (for ending || and also for single element of array.)
-            return true;
+    public static int firstOcc(int arr[], int key, int i) {
+        // Pura funciton else if he hai
+        if (i == arr.length) { // Base case
+            return -1;
         }
-        if (arr[i] > arr[i + 1]) {
-            return false;
-        } // nichi ki run he nhi hoti agr upr wala false dedeta to.
-        return isSorted(arr, i + 1);
-    }
+        if (arr[i] == key) {
+            return i;
+        }
 
+        return firstOcc(arr, key, i + 1);
+    }
 }
+
+// time complexity and space complexity is O(n);
