8668871121a7a332eef249173b835e3f59edad26
Dhawal <dhwalgala05@gmail.com>
2025-12-20 12:38:38 +0530

    Thoda confusing hai . last occurence

--- Recurssion.java
+++ Recurssion.java
@@ -1,8 +1,20 @@
 public class Recurssion {
 
     public static void main(String[] args) { // To find first occurence of an element{key} in an array
-        int[] arr = { 1, 2, 3, 4, 6, 7, 8, 9, 5 };
-        System.out.println(firstOcc(arr, 5, 0));
+        int[] arr = { 1, 2, 3, 4, 6, 7, 8, 3, 5 };
+        System.out.println(lastOcc(arr, 3, 0));
+    }
+
+    public static int lastOcc(int arr[], int key, int i) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int isFound = lastOcc(arr, key, i + 1);
+        if (isFound == -1 && arr[i] == key) {
+            return i;
+        }
+
+        return isFound;
     }
 
     public static int firstOcc(int arr[], int key, int i) {
