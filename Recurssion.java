e23fe0b48966bff129926e11262fb405eb66e6ed
Dhawal <dhwalgala05@gmail.com>
2025-12-22 12:23:40 +0530

    power of any number

--- Recurssion.java
+++ Recurssion.java
@@ -1,33 +1,16 @@
 public class Recurssion {
 
     public static void main(String[] args) { // To find first occurence of an element{key} in an array
-        int[] arr = { 1, 2, 3, 4, 6, 7, 8, 3, 5 };
-        System.out.println(lastOcc(arr, 3, 0));
+        System.out.println(power(2, 10));
     }
 
-    public static int lastOcc(int arr[], int key, int i) {
-        if (i == arr.length) {
-            return -1;
-        }
-        int isFound = lastOcc(arr, key, i + 1);
-        if (isFound == -1 && arr[i] == key) {
-            return i;
+    public static int power(int x, int n) {
+        if (n == 0) { // base case
+            return 1;
         }
 
-        return isFound;
+        return x * power(x, n - 1); // rec8urssion
+
     }
 
-    public static int firstOcc(int arr[], int key, int i) {
-        // Pura funciton else if he hai
-        if (i == arr.length) { // Base case
-            return -1;
-        }
-        if (arr[i] == key) {
-            return i;
-        }
-
-        return firstOcc(arr, key, i + 1);
-    }
 }
-
-// time complexity and space complexity is O(n);
