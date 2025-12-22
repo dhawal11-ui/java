220c5c7aaa2f794034306b1e873e44b99d237945
Dhawal <dhwalgala05@gmail.com>
2025-12-22 14:48:24 +0530

    power of any power optimised way. tc = logn

--- Recurssion.java
+++ Recurssion.java
@@ -1,9 +1,26 @@
 public class Recurssion {
+    // Tc O(logn) Tree daigram >< ( total levels se calc kara ki tc logn hogi )\
+    public static int optimizePower(int a, int n) {
+        if (n == 0) { // Base case
+            return 1;
+        }
+        int halfPowerSq = (int) Math.pow(optimizePower(a, n / 2), 2);
 
-    public static void main(String[] args) { // To find first occurence of an element{key} in an array
-        System.out.println(power(2, 10));
+        // n is odd
+        if (n % 2 != 0) {
+            halfPowerSq = halfPowerSq * a;
+        }
+        return halfPowerSq;
     }
 
+    public static void main(String[] args) { // To find first occurence of an element{key} in an array
+
+        int a = 2;
+        int n = 10;
+        System.out.println(optimizePower(a, n));
+    }
+
+    // Tc O(n);
     public static int power(int x, int n) {
         if (n == 0) { // base case
             return 1;
