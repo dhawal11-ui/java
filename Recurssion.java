1e17a20fd383aa61d2a98281b5663ea00837fd5a
Dhawal <dhwalgala05@gmail.com>
2026-01-01 17:44:42 +0530

    .......

--- Recurssion.java
+++ Recurssion.java
@@ -1,24 +1,19 @@
 public class Recurssion {
 
-    public static void findOcc(int arr[], int idx) {
-
+    public static void converter(int n[], String arr[], int idx) {
         // bc
-        if (idx == 9) {
+        if (idx == arr.length) {
             System.out.println("end");
-
-        } else if (arr[idx] == 2) {
-            System.out.println(idx);
-            findOcc(arr, idx + 1);
-
         } else {
 
-            findOcc(arr, idx + 1);
+            System.out.println(arr[n[idx]]);
+            converter(n, arr, idx + 1);
         }
     }
 
     public static void main(String[] args) {
-        int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2, };
-        findOcc(arr, 0);
-
+        String letter[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        int number[] = { 1, 9, 4, 7 };
+        converter(number, letter, 0);
     }
 }
