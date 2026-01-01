f4fc6a2226ef8cd9c82309a8381691f97325f81c
Dhawal <dhwalgala05@gmail.com>
2026-01-01 18:44:11 +0530

    mergesort ( confusing but interesting) TC= O(nlogn) . Space complexity O(n); (temp array ke liye space ). Depth first technique.

--- Recurssion.java
+++ Recurssion.java
@@ -1,19 +1,67 @@
 public class Recurssion {
 
-    public static void converter(int n[], String arr[], int idx) {
-        // bc
-        if (idx == arr.length) {
-            System.out.println("end");
-        } else {
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
 
-            System.out.println(arr[n[idx]]);
-            converter(n, arr, idx + 1);
+    public static void mergeSort(int arr[], int si, int ei) {
+        // base case
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2; // (si+ei)/2 isse v code chal jata tha
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid + 1, ei); // right part
+        merge(arr, si, mid, ei);
+    }
+
+    public static void merge(int arr[], int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1]; // si = starting index and ei = ending.
+        int i = si; // iterator for left part
+        int j = mid + 1; // iterator for right part
+        int k = 0; // iterator for temperory array.
+
+        // merging temp array.
+        while (i <= mid && j <= ei) {
+            if (arr[i] <= arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // left part ke bache
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right part ke bache
+        while (j <= ei) {
+            temp[k++] = arr[j++];
+        }
+
+        // copy temp to my orginal array
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
         }
     }
 
     public static void main(String[] args) {
-        String letter[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-        int number[] = { 1, 9, 4, 7 };
-        converter(number, letter, 0);
+        /*
+         * Merge sort. TC nlogn
+         * approach
+         * 1> Div mid
+         * 2> merge sort left and right
+         * 3> merge the both left and right part
+         */
+        int arr[] = { 6, 3, 9, 5, 2, 8 };
+        mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
+
 }
