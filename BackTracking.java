05da8a5bebaec5fb2cc9e518a2b3a11d4e38b043
Dhawal <dhwalgala05@gmail.com>
2026-01-17 19:44:55 +0530

    find permutatios using for loop and recussion . ................... tc = n*n

--- BackTracking.java
+++ BackTracking.java
@@ -1,34 +1,22 @@
 public class BackTracking {
 
-    public static void findSubsets(String str, String ans, int i) {
+    public static void findPermutation(String str, String ans) {
         // base case
-        if (i == str.length()) {
-            if (ans.length() == 0) {
-                System.out.println("null");
-            } else {
-                System.out.println(ans);
-            }
-
+        if (str.length() == 0) {
+            System.out.println(ans);
             return;
         }
-        // recurrision
-        // yes choice
-        findSubsets(str, ans + str.charAt(i), i + 1);
-        // no choice
-        findSubsets(str, ans, i + 1);
-        // to print in reverse order exchange the above line positions.
+        // recurrsion
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String NewStr = str.substring(0, i) + str.substring(i + 1); // end me nhi likhe toh str.length -1 hojata re
+            findPermutation(NewStr, ans + curr);
+        }
     }
 
     public static void main(String[] args) {
-        String str = "abcfgsfgsfgs";
-        findSubsets(str, "", 0);
+        String str = "abc";
+        findPermutation(str, "");
     }
 
 }
-
-// TC = O(n*2**n) // ek subset nikal ne ko 1 step lagta then 2**n subsets ke n
-// steps.
-// liye n2**n
-// SC =O(n) // stack meh n levels bante hai ...
-// Also try with string builder . then use deleete character to delete from
-// string.
