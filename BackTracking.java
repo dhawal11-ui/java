982e6dd94bf8720c530941e4372418e6ae732ed8
Dhawal <dhwalgala05@gmail.com>
2026-01-28 08:42:30 +0530

    nQueens and a confusion code at first.

--- BackTracking.java
+++ BackTracking.java
@@ -1,22 +1,76 @@
 public class BackTracking {
+    static int count = 0;
 
-    public static void findPermutation(String str, String ans) {
-        // base case
-        if (str.length() == 0) {
-            System.out.println(ans);
+    public static boolean isSafe(char board[][], int row, int col) {
+        // vertically upp
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left up
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right up
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("___________Chess Board_______");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void nQueens(char board[][], int row) {
+        // Base case
+        if (row == board.length) {
+            printBoard(board);
+            count++;
             return;
         }
-        // recurrsion
-        for (int i = 0; i < str.length(); i++) {
-            char curr = str.charAt(i);
-            String NewStr = str.substring(0, i) + str.substring(i + 1); // end me nhi likhe toh str.length -1 hojata re
-            findPermutation(NewStr, ans + curr);
+
+        // column loop
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1); // function call
+                board[row][j] = 'x'; // backtracking stelp
+            }
+
         }
     }
 
     public static void main(String[] args) {
-        String str = "abc";
-        findPermutation(str, "");
+        int n = 5;
+        char board[][] = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+        nQueens(board, 0);
+        System.out.println("Total ways to solve nQueens = " + count);
     }
 
 }
+
+// Tc = O(n**n) -- without isSafe
+// Tc = O(n!) -- with isSafe -- T(n) = 1QueenPlace * T(n-1) + isSafe() ... Time
+// required for all of this is our requerence relation.
+
+// If we asked to find the number of ways so base case per jakr (count variable)
+// ++ krte jaoooo.
