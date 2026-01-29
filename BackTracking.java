38e6c4ae2d5a91bdd7699d2e30db477aa1527cbe
Dhawal <dhwalgala05@gmail.com>
2026-01-29 19:20:33 +0530

    Grid ways code (higher time complexity )

--- BackTracking.java
+++ BackTracking.java
@@ -1,84 +1,24 @@
 public class BackTracking {
 
-    public static boolean isSafe(char board[][], int row, int col) {
-        // vertically upp
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
+    // Grid.
+    public static int gridWays(int i, int j, int n, int m) {
+        // bse case
+        if (i == n - 1 && j == m - 1) { // condition for last cell
+            return 1;
+        } else if (i == n || j == n) { // bundary cross condition
+            return 0;
         }
 
-        // diagonal left up
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        // diagonal right up
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q')
-                return false;
-        }
-
-        return true;
-    }
-
-    public static void printBoard(char board[][]) {
-        System.out.println("___________Chess Board_______");
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static boolean nQueens(char board[][], int row) {
-        // Base case
-        if (row == board.length) {
-            printBoard(board);
-
-            return true;
-        }
-
-        // column loop
-        for (int j = 0; j < board.length; j++) {
-            if (isSafe(board, row, j)) {
-                board[row][j] = 'Q';
-                if (nQueens(board, row + 1)) {
-                    return true;
-                }
-                ; // function call
-                board[row][j] = 'x'; // backtracking step
-            }
-
-        }
-        return false;
+        int way1 = gridWays(i + 1, j, n, m);
+        int way2 = gridWays(i, j + 1, n, m);
+        return way1 + way2;
     }
 
     public static void main(String[] args) {
-        int n = 2;
-        char board[][] = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                board[i][j] = 'x';
-            }
-        }
-        if (nQueens(board, 0)) {
-            System.out.println("solution is possible");
-
-        } else {
-            System.out.println("solution is not possible");
-        }
-
+        int n = 3, m = 3;
+        System.out.println(gridWays(0, 0, n, m));
     }
-
 }
 
-// Tc = O(n**n) -- without isSafe
-// Tc = O(n!) -- with isSafe -- T(n) = 1QueenPlace * T(n-1) + isSafe() ... Time
-// required for all of this is our requerence relation.
-
-// If we asked to find the number of ways so base case per jakr (count variable)
-// ++ krte jaoooo.
+// Tc O(n*n); X
+// TC O(2**n+m);
